af9143bc3178508bc0945ac92c5011c0d020ed9a
tarek <taga61188@gmail.com>
2024-01-04 03:16:14 -0800

    binary_trees

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - check if node is root
+ * @node: that node we chick it
+ *
+ * Return: 0 or 1
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node->parent != NULL)
+		return (0);
+
+	return (1);
+}
