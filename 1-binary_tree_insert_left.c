2c9b1eaaeb2eed40ef4aa8621ce9100a2169e899
tarek <taga61188@gmail.com>
2024-01-04 02:33:30 -0800

    binary_trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,7 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(binary_tree_t));
+	new = binary_tree_node(parent, value);
 	if (new == NULL)
 		return (NULL);
 
