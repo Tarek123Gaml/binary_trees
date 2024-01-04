9bb3eba722ee74d9e55ec2851dc25d3847bf3390
tarek <taga61188@gmail.com>
2024-01-04 02:28:50 -0800

    binary_trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,7 +14,7 @@
  *              the left-child of the new node.
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
